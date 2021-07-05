ccd3bd0be43de4b0e65651ae25790023bba8abf1
성정준 <sungjungjun@seongjeongjun-ui-MacBookPro.local>
2021-07-05 18:21:02 +0900

    initial setting

---
+++ src/Routes.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// #PAGES
+import Home from './Pages/Home/Home';
+import Detail from './Pages/Detail/Detail';
+
+// #COMPONENTS
+import Nav from './Components/Nav/Nav';
+import Footer from './Components/Footer/Footer';
+
+class Routes extends Component {
+  render() {
+    return (
+      <>
+        <Router>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/detail" component={Detail} />
+          </Switch>
+          <Footer />
+        </Router>
+      </>
+    );
+  }
+}
+
+export default Routes;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// #ROUTES
+import Routes from './Routes';
+
+// #STYLES
+import './Styles/common.scss';
+import './Styles/reset.scss';
+
+ReactDOM.render(<Routes />, document.getElementById('root'));
