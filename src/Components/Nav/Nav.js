e8e8a68c606614c6c74f55d004a8cb9176621d4d
jungjun-Sung <jungzkxm@gmail.com>
2021-07-08 12:53:51 +0900

    newNav branch setting1

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,7 +1,99 @@
 import React, { Component } from 'react';
 
+// ##LIBRARY
+import { Link } from 'react-router-dom';
+
+// ##COMPONENT
+import Login from './NavLogin/Login';
+import NavSearch from './NavSearch/NavSearch';
+import Signup from './NavSignup/Signup';
+
+// ##STYLES
+import './Nav.scss';
+
+import { SEARCH_APIKEY } from '../../Config/Config';
+
 export default class Nav extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoginModal: false,
+      isSignupModal: false,
+      isInputFocused: false,
+      searchLists: [],
+    };
+  }
+  onInputChange = e => {
+    fetch(SEARCH_APIKEY, {
+      method: 'POST',
+      body: JSON.stringify({
+        word: e.target.value,
+      }),
+    })
+      .then(res => res.json())
+      .then(res =>
+        this.setState({
+          searchLists: res,
+        })
+      );
+  };
+
+  closeMoal = () => {
+    this.setState({ isLoginModal: false, isSignupModal: false });
+  };
+
   render() {
-    return <div></div>;
+    const { isLoginModal, isSignupModal, searchLists } = this.state;
+    return (
+      <nav className="nav">
+        <div className="navContainer">
+          <div className="navLeft">
+            <Link to="/" className="navLogoBox">
+              <img
+                alt="companyLogo"
+                src="/images/company-logo.png"
+                className="companyLogo"
+              />
+            </Link>
+            <div className="navCategoryBox">
+              <Link to="/" className="cateAll">
+                전체
+              </Link>
+              <Link to="/evaluating" className="navEvaluation">
+                평가하기
+              </Link>
+            </div>
+          </div>
+          <div className="navRight">
+            <div className="inputBox">
+              <input
+                type="text"
+                className="navInput"
+                placeholder="🔍 작품 제목, 배우, 감독을 검색해보세요."
+                onChange={this.onInputChange}
+              />
+              <NavSearch searchLists={searchLists} />
+            </div>
+            <div className="adminBox">
+              <button
+                className="loginBtn"
+                onClick={() => this.setState({ isLoginModal: true })}
+              >
+                로그인
+              </button>
+              <button
+                className="signupBtn"
+                onClick={() => this.setState({ isSignupModal: true })}
+              >
+                회원가입
+              </button>
+            </div>
+          </div>
+        </div>
+        {isLoginModal && <Login closeMoal={this.closeMoal} />}
+        {isSignupModal && <Signup closeMoal={this.closeMoal} />}
+      </nav>
+    );
   }
 }
---
+++ src/Components/Nav/NavSearch/NavSearch.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './NavSearch.scss';
+
+class NavSearch extends Component {
+  render() {
+    return (
+      <div className="navSearchBox">
+        <p className="searchTitle">연관검색어</p>
+        <ul className="searchResults">
+          {this.props.searchLists.results &&
+            this.props.searchLists.results.map(el => {
+              return <li>{el.word}</li>;
+            })}
+        </ul>
+      </div>
+    );
+  }
+}
+export default NavSearch;
