8a66032ec29c2617134282c365cceb1710577151
dsd <sadad@naver.com>
2021-07-09 23:57:10 +0900

    [Add] 별점기능 호버,클릭,취소 기능 구현

--- src/Components/StarRating/StarRating.js
+++ src/Components/StarRating/StarRating.js
@@ -7,10 +7,13 @@ export default class StarRating extends Component {
     super(props);
     this.state = {
       rateValue: [false, false, false, false, false],
+      hoverRateValue: [false, false, false, false, false],
+      isHover: false,
     };
   }
 
   handleStarClick = clickedIndex => {
+    console.log('handleStarClick');
     const prevRateValue = [...this.state.rateValue];
     const isClickedStarActive = prevRateValue[clickedIndex];
     const isNextStarActive = prevRateValue[clickedIndex + 1];
@@ -19,6 +22,8 @@ export default class StarRating extends Component {
       prevRateValue.fill(false, clickedIndex + 1);
 
       this.setState({
+        isHover: false,
+        hoverRateValue: [false, false, false, false, false],
         rateValue: prevRateValue,
       });
 
@@ -29,6 +34,8 @@ export default class StarRating extends Component {
       prevRateValue.fill(false, 0, clickedIndex + 1);
 
       this.setState({
+        isHover: false,
+        hoverRateValue: [false, false, false, false, false],
         rateValue: prevRateValue,
       });
 
@@ -39,6 +46,8 @@ export default class StarRating extends Component {
       prevRateValue.fill(true, 0, clickedIndex + 1);
 
       this.setState({
+        isHover: false,
+        hoverRateValue: [false, false, false, false, false],
         rateValue: prevRateValue,
       });
 
@@ -46,20 +55,87 @@ export default class StarRating extends Component {
     }
   };
 
+  handleStarMousehover = hoveredIndex => {
+    // console.log('MOUSE hover');
+    const prevRateValue = [...this.state.hoverRateValue];
+    const isClickedStarActive = prevRateValue[hoveredIndex];
+    const isNextStarActive = prevRateValue[hoveredIndex + 1];
+
+    if (isClickedStarActive && isNextStarActive) {
+      prevRateValue.fill(false, hoveredIndex + 1);
+
+      this.setState({
+        isHover: true,
+        hoverRateValue: prevRateValue,
+      });
+
+      return;
+    }
+
+    if (isClickedStarActive) {
+      prevRateValue.fill(false, 0, hoveredIndex + 1);
+
+      this.setState({
+        isHover: true,
+        hoverRateValue: prevRateValue,
+      });
+
+      return;
+    }
+
+    if (!isClickedStarActive) {
+      prevRateValue.fill(true, 0, hoveredIndex + 1);
+
+      this.setState({
+        isHover: true,
+        hoverRateValue: prevRateValue,
+      });
+
+      return;
+    }
+  };
+
+  handleStarMouseout = () => {
+    // console.log('MOUSE OUT');
+
+    this.setState({
+      isHover: false,
+      hoverRateValue: [false, false, false, false, false],
+    });
+  };
+
+  checkIsActive = star => {
+    if (this.state.isHover) {
+      if (this.state.hoverRateValue[star]) {
+        return 'activeStar';
+      }
+
+      return 'inactiveStar';
+    }
+
+    if (this.state.rateValue[star]) {
+      return 'activeStar';
+    }
+
+    return 'inactiveStar';
+  };
+
   render() {
     const starArray = [0, 1, 2, 3, 4];
+    // console.log(this.state);
 
     return (
       <>
         <div className="starList">
           {starArray.map((star, index) => {
             return (
-              <button onClick={() => this.handleStarClick(star)}>
-                <Star
-                  key={index}
-                  size="50"
-                  name={this.state.rateValue[star] ? 'yesStar' : 'noStar'}
-                />
+              <button
+                key={index}
+                onClick={() => this.handleStarClick(star)}
+                onMouseEnter={() => this.handleStarMousehover(star)}
+                onMouseLeave={() => this.handleStarMouseout()}
+              >
+                <Star size="60" name={this.checkIsActive(star)} />
               </button>
             );
           })}
