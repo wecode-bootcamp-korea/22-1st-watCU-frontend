4b131fcb689ce5f83d65260df2997bf5ef09a862
dsd <sadad@naver.com>
2021-07-09 10:17:07 +0900

    [Add] 별점 클릭기능 구현 -원하는 별점  클릭시 해당하는 별점이 매겨집니다. -클릭했던 별점을 다시 클릭하면 초기화가 됩니다. -마우스 호버효과는 미구현입니다.

--- src/Components/StarRating/StarRating.js
+++ src/Components/StarRating/StarRating.js
@@ -5,9 +5,46 @@ import './StarRating.scss';
 export default class StarRating extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      rateValue: [false, false, false, false, false],
+    };
   }
 
-  handleStarClick = () => {};
+  handleStarClick = clickedIndex => {
+    const prevRateValue = [...this.state.rateValue];
+    const isClickedStarActive = prevRateValue[clickedIndex];
+    const isNextStarActive = prevRateValue[clickedIndex + 1];
+
+    if (isClickedStarActive && isNextStarActive) {
+      prevRateValue.fill(false, clickedIndex + 1);
+
+      this.setState({
+        rateValue: prevRateValue,
+      });
+
+      return;
+    }
+
+    if (isClickedStarActive) {
+      prevRateValue.fill(false, 0, clickedIndex + 1);
+
+      this.setState({
+        rateValue: prevRateValue,
+      });
+
+      return;
+    }
+
+    if (!isClickedStarActive) {
+      prevRateValue.fill(true, 0, clickedIndex + 1);
+
+      this.setState({
+        rateValue: prevRateValue,
+      });
+
+      return;
+    }
+  };
 
   render() {
     const starArray = [0, 1, 2, 3, 4];
@@ -17,11 +54,13 @@ export default class StarRating extends Component {
         <div className="starList">
           {starArray.map((star, index) => {
             return (
-              <Star
-                key={index}
-                size="50"
-                onClick={this.handleStarClick(star)}
-              />
+              <button onClick={() => this.handleStarClick(star)}>
+                <Star
+                  key={index}
+                  size="50"
+                  name={this.state.rateValue[star] ? 'yesStar' : 'noStar'}
+                />
+              </button>
             );
           })}
         </div>
